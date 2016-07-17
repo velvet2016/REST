864874be67c3d3ea67333ae893daefd3e6eef33a
velvet2016 <astudzinsky@gmail.com>
2016-07-17 23:25:50 +0300

    changed: type DATE to DATETIME for column purchase_date in table purchase

--- src/main/resources/sql/create_purchase_table.sql
+++ src/main/resources/sql/create_purchase_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE PURCHASE(
 purchase_id INT NOT NULL AUTO_INCREMENT,
 product_id INT NOT NULL,
 quantity INT NOT NULL,
-purchase_date DATE NOT NULL,
+purchase_date DATETIME NOT NULL,
 PRIMARY KEY (purchase_id),
 FOREIGN KEY (product_id) REFERENCES PRODUCT(product_id)
 )
