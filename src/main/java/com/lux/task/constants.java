122f4bb5b136ace014e6bd59698f5351e7d41c7e
velvet2016 <astudzinsky@gmail.com>
2016-07-18 18:21:16 +0300

    fixing constants step1

--- src/main/java/com/lux/task/constants.java
+++ src/main/java/com/lux/task/constants.java
@@ -2,7 +2,7 @@ package com.lux.task;
 
 import java.text.SimpleDateFormat;
 
-public class Constants {
+public class constants {
     public static final String REPORT_PARAMETER_SHOULD_BE_POSITIVE_INTEGER = "Report parameter should be positive integer";
     public static final String DATE_FORMAT = "dd.MM.yyyy";
     public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT);
