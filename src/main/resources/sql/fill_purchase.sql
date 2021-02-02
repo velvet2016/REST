976b640bfa599fc81f7c0e9ac3fb25576ac6576d
astudzynskyi <astudzynskyi@cybervisiontech.com>
2021-02-02 18:24:37 +0200

    fix

--- src/main/resources/sql/fill_purchase.sql
+++ src/main/resources/sql/fill_purchase.sql
@@ -1,7 +1,7 @@
-insert into PURCHASES (product, quantity, purchase_date) values ('banana', 1, STR_TO_DATE('04/31/2004', '%m/%d/%Y') );
-insert into PURCHASES (product, quantity, purchase_date) values ('banana', 2, STR_TO_DATE('04/30/2004', '%m/%d/%Y') );
-insert into PURCHASES (product, quantity, purchase_date) values ('apple', 10, STR_TO_DATE('04/29/2004', '%m/%d/%Y') );
-insert into PURCHASES (product, quantity, purchase_date) values ('kiwi', 10, STR_TO_DATE('04/29/2016', '%m/%d/%Y') );
-insert into PURCHASES (product, quantity, purchase_date) values ('apple', 10, STR_TO_DATE('04/29/2016', '%m/%d/%Y') );
-insert into PURCHASES (product, quantity, purchase_date) values ('apple', 10, STR_TO_DATE('05/29/2016', '%m/%d/%Y') );
-insert into PURCHASES (product, quantity, purchase_date) values ('apple', 10, STR_TO_DATE('06/29/2016', '%m/%d/%Y') );
+insert into PURCHASE (product_id, quantity, purchase_date) values (1, 1, STR_TO_DATE('04/30/2020', '%m/%d/%Y') );
+insert into PURCHASE (product_id, quantity, purchase_date) values (2, 2, STR_TO_DATE('04/30/2004', '%m/%d/%Y') );
+insert into PURCHASE (product_id, quantity, purchase_date) values (3, 10, STR_TO_DATE('04/29/2004', '%m/%d/%Y') );
+insert into PURCHASE (product_id, quantity, purchase_date) values (4, 10, STR_TO_DATE('04/29/2016', '%m/%d/%Y') );
+insert into PURCHASE (product_id, quantity, purchase_date) values (5, 10, STR_TO_DATE('04/29/2016', '%m/%d/%Y') );
+insert into PURCHASE (product_id, quantity, purchase_date) values (3, 10, STR_TO_DATE('05/29/2016', '%m/%d/%Y') );
+insert into PURCHASE (product_id, quantity, purchase_date) values (5, 10, STR_TO_DATE('06/29/2016', '%m/%d/%Y') );
