11d9cce612a2524cf61164e31c2112883651971c
astudzynskyi <astudzynskyi@cybervisiontech.com>
2021-02-02 20:34:59 +0200

    add post product

--- src/main/java/com/lux/task/controllers/RestWsController.java
+++ src/main/java/com/lux/task/controllers/RestWsController.java
@@ -3,26 +3,24 @@ package com.lux.task.controllers;
 
 import com.lux.task.dao.models.Product;
 import com.lux.task.dao.models.Purchase;
-import com.lux.task.dao.models.ReportArguments;
 import com.lux.task.dao.models.ReportLine;
 import com.lux.task.dao.services.ProductService;
 import com.lux.task.dao.services.PurchaseService;
 import com.lux.task.dao.services.ReportService;
 import com.lux.task.exceptions.IncompatibleReportParameterException;
-import com.lux.task.validators.ReportArgumentsValidator;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-
-import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 public class RestWsController {
@@ -37,7 +35,7 @@ public class RestWsController {
     @Autowired
     private ProductService productService;
 
-    @RequestMapping(value = "/rest/report", method = RequestMethod.GET, /*headers="Accept=application/json",*/ produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "report", method = RequestMethod.GET, /*headers="Accept=application/json",*/ produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<ReportLine> getReport(@RequestParam("monthNumber") int monthNumber)  {
         if (monthNumber < 1) {
             throw new IncompatibleReportParameterException();
@@ -45,10 +43,24 @@ public class RestWsController {
         return reportService.getReport(monthNumber);
     }
 
-    @RequestMapping(value = "/rest/add_purchases", method = RequestMethod.POST, headers="Accept=application/json",
+    @RequestMapping(value = "purchases", method = RequestMethod.POST, headers = "Accept=application/json",
             produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public List<Purchase> addPurchases(@RequestBody List<Purchase> purchases)  {
-        return purchaseService.storePurchases(purchases);
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void addPurchases(@RequestBody List<Purchase> purchases) {
+        purchaseService.storePurchases(purchases);
+    }
+
+    @RequestMapping(value = "products", method = RequestMethod.GET, headers = "Accept=application/json",
+            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<Product> getAllProducts() {
+        return productService.getAll();
+    }
+
+    @RequestMapping(value = "product", method = RequestMethod.POST, headers = "Accept=application/json",
+            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void postProduct(@RequestBody Product product) {
+        productService.add(product);
     }
 
 
--- src/main/java/com/lux/task/dao/services/ProductService.java
+++ src/main/java/com/lux/task/dao/services/ProductService.java
@@ -1,14 +1,13 @@
 package com.lux.task.dao.services;
 
 import com.lux.task.dao.ProductDao;
-import com.lux.task.dao.impl.ProductDaoImpl;
 import com.lux.task.dao.models.Product;
 import com.lux.task.dao.models.Purchase;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.util.*;
 
 @Service
 public class ProductService {
@@ -18,12 +17,19 @@ public class ProductService {
 
     public Map<Integer, String> getListForUiSelect(){
         List<Product> allProducts = dao.getAll();
-        List<String> productNamesList = new ArrayList<String>();
         LinkedHashMap<Integer, String> mapForUi = new LinkedHashMap<>();
         for (Product product : allProducts) {
             mapForUi.put(product.getId(), product.getName());
         }
         return mapForUi;
+    }
+
+    public List<Product> getAll() {
+        return dao.getAll();
+    }
+
+    public int add(Product product) {
+        return dao.save(product);
     }
 
     public void fillProductInPurchaseByProductId(Purchase purchase){
